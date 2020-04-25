dd2629a5296243a26f8404e8c9df36d010d20f44
Val P <valp0591@gmail.com>
2020-04-25 16:29:44 +0500

    321

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -39,39 +39,12 @@ const ContactPage = ({
           }}
           >Let me help you kick start your next project</p>
           <div>
-            <form className={contactStyles.formContainer} form method="POST" netlify-honeypot="bot-field" data-netlify="true"> 
+            <form className={contactStyles.formContainer} form method="post" netlify-honeypot="bot-field" data-netlify="true"> 
               <input type="hidden" name="bot-field"/>
-                <Form.Control placeholder="Your name" input type="text" name="name" style={{
-                ...scale(.8),
-                fontFamily: `Raleway, sans-serif`,
-                textTransform: 'uppercase',
-                marginBottom: rhythm(1),
-                color: 'var(--textTitle)',
-                transition: 'color 0.2s ease-out, background 0.2s ease-out',
-                fontWeight: 200,
-                letterSpacing: -1,
-                }}/>
-                <Form.Control placeholder="E-mail" input type="email" name="email" style={{
-                ...scale(.8),
-                fontFamily: `Raleway, sans-serif`,
-                textTransform: 'uppercase',
-                marginBottom: rhythm(1),
-                color: 'var(--textTitle)',
-                transition: 'color 0.2s ease-out, background 0.2s ease-out',
-                fontWeight: 200,
-                letterSpacing: -1,
-                }}/>
-                <Form.Control as="textarea" rows="3" placeholder="Some thoughts about your project to be" input type="text" name="message" style={{
-                ...scale(.8),
-                fontFamily: `Raleway, sans-serif`,
-                textTransform: 'uppercase',
-                marginBottom: rhythm(1),
-                color: 'var(--textTitle)',
-                transition: 'color 0.2s ease-out, background 0.2s ease-out',
-                fontWeight: 200,
-                letterSpacing: -1,
-                }}/>
-                  <hr></hr>
+                <Form.Control placeholder="Your name" input type="text" name="name"/>
+                <Form.Control placeholder="E-mail" input type="email" name="email" />
+                <Form.Control as="textarea" rows="3" placeholder="Some thoughts about your project to be" input type="text" name="message"/>
+                <hr></hr>
               <div className="d-flex justify-content-end">
                 <Button variant="secondary" type="submit" style={{
                         ...scale(.8),
