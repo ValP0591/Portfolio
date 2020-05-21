d853f7c4bdd0d55d99c2c356673bc5f96204deaf
Val P <valp0591@gmail.com>
2020-05-21 22:55:44 +0500

    '112121212121'

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import { rhythm, scale } from "../utils/typography"
-import AniLink from "gatsby-plugin-transition-link/AniLink"
  
 const ContactForm = (props) => (
     <section id="contact">
